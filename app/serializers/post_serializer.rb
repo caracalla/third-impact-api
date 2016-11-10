70f5a9a2b9af986ea3a47b75410ea7917c4dde3f
James Anderson <caracallarex@gmail.com>
2016-11-10 01:09:50 -0800

    Add CORS for front end client

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,10 @@
+class PostSerializer < ActiveModel::Serializer
+  attributes  :id,
+              :title,
+              :content,
+              :user_id,
+              :created_at,
+              :updated_at
+
+  has_one :user
+end
